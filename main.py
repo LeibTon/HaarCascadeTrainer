71a3069e02f312fe8498f34158cb8324a4dc00a2
prakashaditya369 <62632865+prakashaditya369@users.noreply.github.com>
2020-05-30 17:13:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from training import *
 
 temp = HaarTraining("car.jpg")
+links = ["http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=n00021939"]
+temp.store_negative_images(self,links)
 temp.resize_positive_image()
+temp.create_list()
 temp.train(num=1200,numPos=1100,numNeg=550)
